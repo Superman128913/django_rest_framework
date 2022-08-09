532ddc9ca1e57ee5ad9b5fc52f9f171080c0cce0
HICCUP <galaxy20160801@gmail.com>
2022-08-10 06:36:09 +0900

    for serializers.py

--- send_data/serializers.py
+++ send_data/serializers.py
@@ -1,5 +1,4 @@
 from dataclasses import fields
-from typing_extensions import Required
 from rest_framework import serializers
 # from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
@@ -84,8 +83,8 @@ class StockSerializer(serializers.ModelSerializer):
 
 #Serializer for order
 class OrderSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(input_formats='%b %d %Y %H:%M:%S', Required=False)
-    updated_at = serializers.DateTimeField(input_formats='%b %d %Y %H:%M:%S', Required=False)
+    created_at = serializers.DateTimeField(input_formats='%b %d %Y %H:%M:%S', required=False)
+    updated_at = serializers.DateTimeField(input_formats='%b %d %Y %H:%M:%S', required=False)
     class Meta:
         model = Orders
         fields = ["id", "user", "stock", "bid_price", "type", "status", "bid_volume", "executed_volume", "created_at", "updated_at"]
